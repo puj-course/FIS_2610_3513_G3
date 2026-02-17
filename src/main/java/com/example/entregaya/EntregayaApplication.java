3636ce8a4bdd35549b61ac63e93a544643f7a91b
andresj-castrillo <ancastrillo@javeriana.edu.co>
2026-02-17 09:50:33 -0500

    Agrega estructura inicial de Spring Boot

---
+++ src/main/java/com/example/entregaya/EntregayaApplication.java
@@ -0,0 +1,13 @@
+package com.example.entregaya;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EntregayaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EntregayaApplication.class, args);
+	}
+
+}
