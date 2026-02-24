cee765306bf99360187b2aec3043932b18e28f20
barretordaniel <dbarretor1@gmail.com>
2026-02-23 21:27:34 -0500

    fix: ajustar configuración de seguridad (#49)

--- src/main/java/com/example/entregaya/controller/AuthController.java
+++ src/main/java/com/example/entregaya/controller/AuthController.java
@@ -1,7 +1,13 @@
 package com.example.entregaya.controller;
 
+import com.example.entregaya.service.CustomUserDetailsService;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 /**
  * Controlador encargado de manejar las vistas
@@ -9,6 +15,13 @@ import org.springframework.web.bind.annotation.GetMapping;
  */
 @Controller
 public class AuthController {
+
+
+    private final CustomUserDetailsService userDetailsService;
+
+    public AuthController(CustomUserDetailsService UserDetailsService) {
+        this.userDetailsService = UserDetailsService;
+    }
 
     /**
      * Muestra la pagina de login personalizada.
@@ -25,6 +38,22 @@ public class AuthController {
     public String registerForm() {
         return "register"; // retorna register.html
     }
+    @PostMapping("/register")
+    public String register(@RequestParam("username") String username,
+                           @RequestParam("password") String password,
+                           RedirectAttributes redirectAttributes) {
+        try {
+            userDetailsService.register(username, password);
+            redirectAttributes.addFlashAttribute("success", "Registro Completado. Inicia sesión");
+            return "redirect:/login";
+        }catch(IllegalArgumentException il){
+            redirectAttributes.addFlashAttribute("error", il.getMessage());
+            return "redirect:/login";
+        }
+    }
+
+
+
     /**
      * Pagina principal luego de autenticacion.
      */
