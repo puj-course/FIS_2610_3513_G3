d4ac26c124f9a8cb201df8f1b7e4060feae61db3
barretordaniel <dbarretor1@gmail.com>
2026-03-27 14:50:26 -0500

    fix: Se agregaron las librerias faltantes en trabajocontroller (#241)

--- src/main/java/com/example/entregaya/controller/TrabajoController.java
+++ src/main/java/com/example/entregaya/controller/TrabajoController.java
@@ -17,6 +17,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
