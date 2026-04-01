f124f32d7693e7c3b74789fec1a717324391ef04
barretordaniel <dbarretor1@gmail.com>
2026-04-01 13:01:41 -0500

    feat: Se crearon las clases que nos permiten hacer uso del patron strategy. Closes #262

--- src/main/java/com/example/entregaya/strategy/Lideroeditorstrategy.java
+++ src/main/java/com/example/entregaya/strategy/Lideroeditorstrategy.java
@@ -1,4 +1,14 @@
 package com.example.entregaya.strategy;
 
-public class Lideroeditorstrategy {
+
+import com.example.entregaya.model.ColaboradorTrabajo;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Lideroeditorstrategy implements Permisostrategy {
+    @Override
+    public boolean tienePermiso(ColaboradorTrabajo colaborador) {
+        return colaborador.getRol() == ColaboradorTrabajo.Rol.LIDER
+                || colaborador.getRol() == ColaboradorTrabajo.Rol.EDITOR;
+    }
 }
