cbce31cba5a15aa50e1faaa62947cbe114486c66
barretordaniel <dbarretor1@gmail.com>
2026-05-07 10:49:33 -0500

    Taller docker arreglos (#514)

--- src/main/java/com/example/entregaya/model/User.java
+++ src/main/java/com/example/entregaya/model/User.java
@@ -92,7 +92,7 @@ public class User {
     public void setEmail(String email) {
         this.email = email;
     }
-    
+
     public String getTelegramChatId() {
         return telegramChatId;
     }
